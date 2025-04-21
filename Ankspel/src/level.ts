0835a5f1a6c97a107dd328b418542f4efae686d2
lokeig <loke_ohrstrom@hotmail.se>
2025-04-21 18:14:11 +0200

    Delete Ankspel directory

--- Ankspel/src/level.ts
+++
@@ -1,3 +0,0 @@
-import { GameObject } from "./gameobject"
-
-
--- Ankspel/src/types.ts
+++
@@ -1,8 +0,0 @@
-export type Vector = {
-    x: number,
-    y: number
-}
-
-export type Direction = "LEFT" | "RIGHT";
-export const left = "LEFT";
-export const right = "RIGHT";
