548a2b518bde73f33cc800eb013a124dd1fde0d9
Loke Öhrström <loke_ohrstrom@hotmail.se>
2026-03-02 15:13:52 +0100

    lowercase names fix

--- src/Game/Audio/sound.ts
+++ src/Game/Audio/sound.ts
@@ -11,15 +11,15 @@ const Sound = {
 } as const;
 
 const SoundInfo: Record<SoundName, { src: string }> = {
-    glock: { src: "/assets/sounds/glock.mp3" },
-    shotgunFire: { src: "/assets/sounds/shotgunFire.mp3" },
-    shotgunLoad: { src: "/assets/sounds/shotgunLoad.mp3" },
-    explode: { src: "/assets/sounds/explode.mp3" },
-    pullPin: { src: "/assets/sounds/pullPin.mp3" },
-    land: { src: "/assets/sounds/land.mp3" },
-    jump: { src: "/assets/sounds/jump.mp3" },
-    equip: { src: "/assets/sounds/equip.mp3" },
-    beep: { src: "/assets/sounds/beep.mp3" },
+    glock: { src: "/assets/Sounds/glock.mp3" },
+    shotgunFire: { src: "/assets/Sounds/shotgunFire.mp3" },
+    shotgunLoad: { src: "/assets/Sounds/shotgunLoad.mp3" },
+    explode: { src: "/assets/Sounds/explode.mp3" },
+    pullPin: { src: "/assets/Sounds/pullPin.mp3" },
+    land: { src: "/assets/Sounds/land.mp3" },
+    jump: { src: "/assets/Sounds/jump.mp3" },
+    equip: { src: "/assets/Sounds/equip.mp3" },
+    beep: { src: "/assets/Sounds/beep.mp3" },
 };
     
 type SoundName = typeof Sound[keyof typeof Sound];
--- src/Render/images.ts
+++ src/Render/images.ts
@@ -1,34 +1,34 @@
 const Images = {
     // Player
-    player: { src: '/assets/images/player/player.png', frameWidth: 32, frameHeight: 32 },
-    playerHands: { src: '/assets/images/player/playerHands.png', frameWidth: 16, frameHeight: 16 },
+    player: { src: '/assets/Images/Player/player.png', frameWidth: 32, frameHeight: 32 },
+    playerHands: { src: '/assets/Images/Player/playerHands.png', frameWidth: 16, frameHeight: 16 },
 
     // Tiles
-    tileIce: { src: '/assets/images/tiles/tileIce.png', frameWidth: 16, frameHeight: 16 },
-    tileNature: { src: '/assets/images/tiles/natureTileset.png', frameWidth: 16, frameHeight: 16 },
+    tileIce: { src: '/assets/Images/Tiles/tileIce.png', frameWidth: 16, frameHeight: 16 },
+    tileNature: { src: '/assets/Images/Tiles/natureTileset.png', frameWidth: 16, frameHeight: 16 },
     
     // Items
-    prop: { src: '/assets/images/items/props.png', frameWidth: 16, frameHeight: 16 },
-    shotgun: { src: '/assets/images/items/shotguns.png', frameWidth: 32, frameHeight: 32 },
-    glock: { src: '/assets/images/items/glock.png', frameWidth: 20, frameHeight: 20 },
-    grenade: { src: '/assets/images/items/grenade.png', frameWidth: 16, frameHeight: 16 },
-    armor: { src: '/assets/images/items/armor.png', frameWidth: 16, frameHeight: 16 },
+    prop: { src: '/assets/Images/Items/props.png', frameWidth: 16, frameHeight: 16 },
+    shotgun: { src: '/assets/Images/Items/shotguns.png', frameWidth: 32, frameHeight: 32 },
+    glock: { src: '/assets/Images/Items/glock.png', frameWidth: 20, frameHeight: 20 },
+    grenade: { src: '/assets/Images/Items/grenade.png', frameWidth: 16, frameHeight: 16 },
+    armor: { src: '/assets/Images/Items/armor.png', frameWidth: 16, frameHeight: 16 },
 
     // Projectiles
-    trail: { src: '/assets/images/projectiles/trail.png', frameWidth: 8, frameHeight: 1 },
+    trail: { src: '/assets/Images/Projectiles/trail.png', frameWidth: 8, frameHeight: 1 },
 
     // Particles
-    explosion: { src: '/assets/images/vfx/explosion.png', frameWidth: 64, frameHeight: 64 },
-    smallFlare: { src: '/assets/images/vfx/smallFlare.png', frameWidth: 11, frameHeight: 10 },
-    bulletRebound: { src: '/assets/images/vfx/bulletRebound.png', frameWidth: 16, frameHeight: 16 },
-    bulletGlow: { src: '/assets/images/vfx/bulletGlow.png', frameWidth: 1, frameHeight: 5 },
+    explosion: { src: '/assets/Images/VFX/explosion.png', frameWidth: 64, frameHeight: 64 },
+    smallFlare: { src: '/assets/Images/VFX/smallFlare.png', frameWidth: 11, frameHeight: 10 },
+    bulletRebound: { src: '/assets/Images/VFX/bulletRebound.png', frameWidth: 16, frameHeight: 16 },
+    bulletGlow: { src: '/assets/Images/VFX/bulletGlow.png', frameWidth: 1, frameHeight: 5 },
 
     // Background
-    forestBackground: { src: '/assets/images/ParallaxBackgrounds/forest.png', frameWidth: 320, frameHeight: 240 },
+    forestBackground: { src: '/assets/Images/ParallaxBackgrounds/forest.png', frameWidth: 320, frameHeight: 240 },
 
     // UI
-    ready: { src: '/assets/images/ui/ready.png', frameWidth: 41, frameHeight: 9 },
-    get: { src: '/assets/images/ui/get.png', frameWidth: 25, frameHeight: 9 },
+    ready: { src: '/assets/Images/UI/ready.png', frameWidth: 41, frameHeight: 9 },
+    get: { src: '/assets/Images/UI/get.png', frameWidth: 25, frameHeight: 9 },
 } as const;
 
 type ImageName = keyof typeof Images;
