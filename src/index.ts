c0d9fb3fe150b502d02bae7589c8da18bb9b6a32
Loke Öhrström <loke_ohrstrom@hotmail.se>
2026-03-10 20:22:18 +0100

    removed old dated ol maps

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { MainMenuCSS } from "@impl/LobbyList/CSS";
 import { CanvasRender } from "@impl/Render";
 import { MultiPeerServer } from "@impl/Server/WebRTC";
 import { MapManager } from "@game/Map";
-import { defaultMap, defaultMap2, defaultMap3, defaultMap4 } from "@impl/Maps";
+import { defaultMap} from "@impl/Maps";
 import { RequestAnimationFrameTimer } from "@impl/FrameTimer/requestAnimationFrame";
 import { HTMLAudio } from "@impl/Audio/HTMLAudio";
 import { AudioManager } from "@game/Audio/audioManager";
@@ -19,9 +19,6 @@ AudioManager.set(new HTMLAudio());
 
 
 MapManager.addMap(defaultMap);
-// MapManager.addMap(defaultMap2);
-// MapManager.addMap(defaultMap3);
-// MapManager.addMap(defaultMap4);
 registerDefaultNames();
 
 Connection.set(new MultiPeerServer(new WebSocket("https://ankspel.onrender.com")));
