be6fddbb9cd9b97fc54a2f93f5b96945100d757f
Loke Öhrström <loke_ohrstrom@hotmail.se>
2026-05-05 20:25:07 +0200

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        mapEditor: "mapEditor.html",
+      },
+    },
+  },
 });
